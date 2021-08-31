fa28ee2f7f505d7fe73c97da23023e59fd953911
bisrat2020 <wow.bisrat11@gmail.com>
2021-08-31 16:50:42 -0400

    update

---
+++ W1D2_Component/lesson2/my-app/src/Columns.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+
+export class Columns extends Component {
+  render() {
+    return (
+        <React.Fragment>
+          <td>Hello</td>
+         <td>World</td>
+         <td>World</td>
+    </React.Fragment>
+      
+    
+    );
+  }
+}
+
+export default Columns;
